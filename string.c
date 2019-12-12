8e844c4593633032f22ea6593b52f126c3c4b6a3
MoBoustta <moboustta6@gmail.com>
2019-12-12 21:46:34 +0100

    printf

--- string.c
+++ string.c
@@ -13,71 +13,72 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-int     is_string(t_struct *list, va_list ap)
+int		is_string(t_struct *list, va_list ap)
 {
-    char *out_str;
-    int str_len;
+	char	*out_str;
+	int		str_len;
 
-    out_str = (char *)va_arg(ap, char*);
-    if (out_str == NULL)
-        out_str = "(null)";
-    str_len = ft_strlen(out_str);
-    if (str_len == 0)
-        out_str = ft_strdup("");
-    if (list->width == -1 && list->precision == -1 && !list->dot)
-    {
-        list->n += write(1,out_str,str_len);
-        return (0);
-    }
-    else
-    {
-        if (list->minus == 1)
-            goto_right(list, out_str, str_len);
-        else
-            goto_left(list, out_str, str_len);
-    }
-    return (0);
-}
-void    goto_right(t_struct *list, char *out_str, int str_len)
-{
-    if (list->precision > 0 && str_len == 0)
-        list->precision = 0;
-    if ((list->precision > 0 || list->precision == -1) && list->dot == 1)
-        list->n += write(1,out_str, list->precision);
-    else
-        list->n += write(1, out_str, str_len);
-    if (list->precision > str_len)
-        list->precision = str_len;
-    else if (list->dot ==0)
-        list->width -= str_len;
-    while (list->width > list->precision && list->width > 0)
-    {
-        list->n += write(1," ", 1);
-        list->width--;
-    }
+	out_str = (char *)va_arg(ap, char*);
+	if (out_str == NULL)
+		out_str = "(null)";
+	str_len = ft_strlen(out_str);
+	if (str_len == 0)
+		out_str = ft_strdup("");
+	if (list->width == -1 && list->precision == -1 && !list->dot)
+	{
+		list->n += write(1, out_str, str_len);
+		return (0);
+	}
+	else
+	{
+		if (list->minus == 1)
+			goto_right(list, out_str, str_len);
+		else
+			goto_left(list, out_str, str_len);
+	}
+	return (0);
 }
 
-void    goto_left(t_struct *list, char *out_str, int str_len)
+void	goto_right(t_struct *list, char *out_str, int str_len)
 {
-    if (list->dot && str_len > list->precision)
-        str_len = list->precision;
-    if (list->dot == 1)
-        list->precision == -1 ? str_len = 0 : str_len;
-    if (list->width > str_len)
-    {
-        while ((list->width - str_len) > 0)
-        {
-            list->n += write(1," ",1);
-            list->width--;
-        }
-    }
-    if (str_len != 0)
-    {
-        if (list->precision > str_len)
-            list->precision = str_len;
-        if ((list->precision > 0 || list->precision == -1) && list->dot == 1)
-            list->n += write(1,out_str, list->precision);
-        else
-            list->n += write(1, out_str, str_len);
-    }
+	if (list->precision > 0 && str_len == 0)
+		list->precision = 0;
+	if ((list->precision > 0 || list->precision == -1) && list->dot == 1)
+		list->n += write(1, out_str, list->precision);
+	else
+		list->n += write(1, out_str, str_len);
+	if (list->precision > str_len)
+		list->precision = str_len;
+	else if (list->dot == 0)
+		list->width -= str_len;
+	while (list->width > list->precision && list->width > 0)
+	{
+		list->n += write(1, " ", 1);
+		list->width--;
+	}
+}
+
+void	goto_left(t_struct *list, char *out_str, int str_len)
+{
+	if (list->dot && str_len > list->precision)
+		str_len = list->precision;
+	if (list->dot == 1)
+		list->precision == -1 ? str_len = 0 : str_len;
+	if (list->width > str_len)
+	{
+		while ((list->width - str_len) > 0)
+		{
+			list->n += write(1, " ", 1);
+			list->width--;
+		}
+	}
+	if (str_len != 0)
+	{
+		if (list->precision > str_len)
+			list->precision = str_len;
+		if ((list->precision > 0 || list->precision == -1) && list->dot == 1)
+			list->n += write(1, out_str, list->precision);
+		else
+			list->n += write(1, out_str, str_len);
+	}
 }
--- unsigned.c
+++ unsigned.c
@@ -12,78 +12,82 @@
 
 #include "ft_printf.h"
 #include "libft.h"
-int    zero_input_u(t_struct *list, unsigned int out_int)
-{
-    if (out_int == 0 && list->dot == 1 && list->width > 0 && (list->precision == -1 || list->precision == 0))
-    {
-        while (list->width-- > 0)
-            list->n += write(1," ",1);
-        return (1);
-    }
-    else if (out_int == 0 && list->dot == 1 && (list->precision == 0 || list->precision == -1))
-    {
-        write(1,"",1);
-        return (1);
-    }
-    return (0);
-}
-int     is_unsigned(t_struct *list, va_list ap)
-{
-    unsigned int out_unsigned;
-    unsigned int unsigned_len;
-    char *s;
 
-    out_unsigned = (unsigned int)va_arg(ap, unsigned int);
-    unsigned_len = ft_intlen(out_unsigned);
-    s = ft_itoa(out_unsigned);
-    if (zero_input(list, out_unsigned) == 1)
-        return (0);
-    if (list->width == -1 && list->precision == -1)
-    {
-        list->n += write(1, s , unsigned_len);
-        return (0);
-    }
-    else
-    {
-        widthprecisionhandle(list, unsigned_len);
-        if (list->minus == 1)
-            unsigned_rightparese(list, s, unsigned_len);
-        else
-            unsigned_leftparese(list, s, unsigned_len);
-    }
-    return (0);
+int		zero_input_u(t_struct *list, unsigned int out_int)
+{
+	if (out_int == 0 && list->dot == 1 && list->width > 0 &&
+	(list->precision == -1 || list->precision == 0))
+	{
+		while (list->width-- > 0)
+			list->n += write(1, " ", 1);
+		return (1);
+	}
+	else if (out_int == 0 && list->dot == 1 &&
+		(list->precision == 0 || list->precision == -1))
+	{
+		write(1, "", 1);
+		return (1);
+	}
+	return (0);
 }
 
-void    widthprecisionhandle(t_struct *list, unsigned int unsigned_len)
+int		is_unsigned(t_struct *list, va_list ap)
 {
-    if (list->dot && list->precision > (int)unsigned_len)
-        list->precision -= unsigned_len;
-    else
-        list->precision = 0;
-    list->width = list->width - (list->precision + unsigned_len);
+	unsigned int	out_unsigned;
+	unsigned int	unsigned_len;
+	char			*s;
+
+	out_unsigned = (unsigned int)va_arg(ap, unsigned int);
+	unsigned_len = ft_intlen(out_unsigned);
+	s = ft_itoa(out_unsigned);
+	if (zero_input(list, out_unsigned) == 1)
+		return (0);
+	if (list->width == -1 && list->precision == -1)
+	{
+		list->n += write(1, s, unsigned_len);
+		return (0);
+	}
+	else
+	{
+		widthprecisionhandle(list, unsigned_len);
+		if (list->minus == 1)
+			unsigned_rightparese(list, s, unsigned_len);
+		else
+			unsigned_leftparese(list, s, unsigned_len);
+	}
+	return (0);
 }
 
-void    unsigned_rightparese(t_struct *list,char *s, unsigned int unsigned_len)
+void	widthprecisionhandle(t_struct *list, unsigned int unsigned_len)
 {
-    while(list->precision-- > 0)
-        list->n += write(1,"0",1);
-    list->n += write(1, s, unsigned_len);
-    while (list->width-- > 0)
-        list->n += write(1," ",1);
+	if (list->dot && list->precision > (int)unsigned_len)
+		list->precision -= unsigned_len;
+	else
+		list->precision = 0;
+	list->width = list->width - (list->precision + unsigned_len);
 }
 
-void    unsigned_leftparese(t_struct *list,char *s, unsigned int unsigned_len)
+void	unsigned_rightparese(t_struct *list, char *s, unsigned int unsigned_len)
 {
-    int rc;
+	while (list->precision-- > 0)
+		list->n += write(1, "0", 1);
+	list->n += write(1, s, unsigned_len);
+	while (list->width-- > 0)
+		list->n += write(1, " ", 1);
+}
 
-    rc = '\0';
-    if (list->zero && !list->dot)
-        rc = '0';
-    else
-        rc = ' ';
-    while(list->width-- > 0)
-        list->n += write(1,&rc,1);
-    while(list->precision-- > 0)
-        list->n += write(1,"0",1);
-    list->n += write(1, s , unsigned_len);
+void	unsigned_leftparese(t_struct *list, char *s, unsigned int unsigned_len)
+{
+	int rc;
+
+	rc = '\0';
+	if (list->zero && !list->dot)
+		rc = '0';
+	else
+		rc = ' ';
+	while (list->width-- > 0)
+		list->n += write(1, &rc, 1);
+	while (list->precision-- > 0)
+		list->n += write(1, "0", 1);
+	list->n += write(1, s, unsigned_len);
 }
